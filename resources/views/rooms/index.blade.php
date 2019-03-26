e77d40911a7c0e59ac9f639093f5a6d931133691
Thanh Lai <lainguyen523@gmail.com>
2019-03-26 14:48:50 +0700

    finish validation rooms

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -3,49 +3,50 @@
 @section('title', 'Room manage')
 
 @section('body')
-<center>
-    <h2>Rooms</h2>
-</center>
-<div class="container-fluid">
-        <button class="btn btn-primary"><a href="{{url('rooms/create')}}">Add new</a></button>
+
+    <div class="container-fluid">
+
+        <button class="bt label-warning"><a href="{{url('rooms/create')}}" class="coll">ADD NEW</a></button>
+        
     </div><br>
-   
-	 
+    <div class="container-fluid text-center">
         <table class="table table-bordered">
     
-    <thead>
-        <tr>
-            <th>STT</th>
-            <th>Name</th>
-            <th>desc</th>
-            <th>status</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php $i = 0; ?>
-        @foreach($roomsList as $rooms)
-        <?php $i = $i + 1; ?>
-        <tr>
-            <td>{{$i}}</td>
-            <td>{{ $rooms->name }}</td>
-            <td>{{ $rooms->desc }}</td>
-            <td>{{ RoomsHelper::getStatus($rooms->status) }}</td>
-            <td><button class="btn btn-secondary"> 
-                <a href="{{ url("rooms/$rooms->id/edit") }}">Edit</a>
-            </button></td>
-            <td>
-                <form action="{{ url('rooms/'.$rooms->id) }}" method="POST"> 
-                    {{ csrf_field() }}
-                    {{ method_field('DELETE') }}
-                
-                   <button class="btn btn-primary" type="submit" data-toggle="tooltip" title="Delete" data-placement="top" onclick="return confirm('bạn có thực sự muốn xóa ?'); ">Delete</button>
-                </form>
-        </tr>
-    </td>
-        @endforeach
-    </tbody>
-</table>
-
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Name</th>
+                    <th>desc</th>
+                    <th>status</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $i = 0; ?>
+                @foreach($roomsList as $rooms)
+                <tr>
+                    <td>
+                         @php($i++)
+                         {{ RoomsHelper::increment($i, $roomsList->perPage(), $roomsList->currentPage())}}
+                    </td>
+                    <td>{{ $rooms->name }}</td>
+                    <td>{{ $rooms->desc }}</td>
+                    <td>{{ RoomsHelper::getStatus($rooms->status) }}</td>
+                    <td><button class="btn btn-secondary"> 
+                        <a href="{{ url("rooms/$rooms->id/edit") }}">Edit</a>
+                    </button></td>
+                    <td>
+                        <form action="{{ url('rooms/'.$rooms->id) }}" method="POST"> 
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                        
+                           <button class="btn btn-primary" type="submit" data-toggle="tooltip" title="Delete" data-placement="top" onclick="return confirm('bạn có thực sự muốn xóa ?'); ">Delete</button>
+                        </form>
+                    </tr>
+                </td>
+            @endforeach
+        </tbody>
+    </table>
+ {{ $roomsList}}
 @endsection
