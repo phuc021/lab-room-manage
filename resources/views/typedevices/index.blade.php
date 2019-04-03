ef493d57c9d71e9d0b6decb43c90ec24ef557439
Thanh <pentapperthanh@gmail.com>
2019-04-03 09:11:33 +0700

    finish style css

--- resources/views/typedevices/index.blade.php
+++ resources/views/typedevices/index.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 
-@section('title', 'TypeDevices')
+@section('title', ' List TypeDevices')
 	
 
 @section('body')
@@ -8,29 +8,29 @@
 	<h1 class="text-center" >{{trans('TYPEDEVICE')}}</h1>
 
 	<div class="container-fluid">
-		<button class="btn btn-default add-new-tdv" value="Add"><a href="{{ url('typedevices/create') }}">{{trans('ADD')}}</a></button>
+		<button id="add-new-tdv" class="btn btn-default add-new-tdv" value="Add"><a href="{{ url('typedevices/create') }}">{{trans('ADD')}}</a></button>
 	</div>
 
 	<div class="container-fluid text-center">
-		<table class="table table-bordered">
+		<table class="table table-bordered list-typedevice">
 			<thead>
 				<tr>
-					<th>Stt</th>
-					<th>Name</th>
-					<th>Edit</th>
-					<th>Delete</th>
+					<th><center>Stt</center></th>
+					<th><center>Name</center></th>
+					<th><center>Edit</center></th>
+					<th><center>Delete</center></th>
 
 				</tr>
 			</thead>
-			<?php $i = 0; ?>
+			@php($i = 0)
+			@foreach($typedevicesList as $typedevice)
 			<tbody>
-				@foreach($typedevicesList as $typedevice)
-				<tr>
+				<tr @if($i% 2 == 0) class="old" @else class="even" @endif >
 					<td><?php echo ++$i; ?></td>
 					<td><p>{{$typedevice->name}}</p></td></p></td>
 
-					<td><button class="btn btn-default" title="Edit" type="submit" value="Edit"><a href="{{ url('typedevices') }}/{{$typedevice->id}}/edit">Edit</a></button></td>
-					<td>
+					<td id="button-option-edi-tdv"><button class="btn btn-default" title="Edit" type="submit" value="Edit"><a href="{{ url('typedevices') }}/{{$typedevice->id}}/edit">Edit</a></button></td>
+					<td id="button-option-del-tdv">
 						<form action="{{ url('typedevices/'.$typedevice->id) }}" method="POST">
 							@csrf
 							{{ method_field('DELETE') }}
@@ -41,6 +41,6 @@
 				@endforeach
 			</tbody>
 		</table>
-		{{ $typedevicesList }}
+		{{ $typedevicesList->links() }}
 	</div>
 @endsection
