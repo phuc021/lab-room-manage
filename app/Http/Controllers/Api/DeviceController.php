7a4f8ac9694697475f9d71b9871a4b29fe81399b
Van Phuc <v.phuc021@gmail.com>
2019-04-09 17:06:20 +0700

    finish update api device 2

--- app/Http/Controllers/Api/DeviceController.php
+++ app/Http/Controllers/Api/DeviceController.php
@@ -15,50 +15,14 @@ class DeviceController extends BaseController
 {
     public function index()
     {
-        $deviceList = DB::table('devices')->orderBy('id','DESC')->paginate(20);
-        return api_success(['data' => $deviceList]);
+        $deviceList = DB::table('devices')->orderBy('id','DESC')->get();
+        return api_success(['data' => $deviceList],200);
     }
 
-    public function store(Request $request)
-    {
-        $devices = Device::create($request->all());
-
-        if($devices){
-            return api_errors(400,['errors' => 'This device does not exist']);
-        } else {
-            return api_success(['devices' => $devices]);
-        }
-        
-    }
-
-    public function update(DeviceRequest $request, $id)
-    {
-        //
-        $devices = Device::findOrFail($id);
-        $validate = $request->validate([
-           
-            'name' => 'required',
-            'desc' => 'required',
-            'status' => 'required',
-            'computers_id' => 'required', 
-            'type_devices_id' => 'required',
-        ]);
-        $devices->update($request->all());
-        if ($validate->fails()) {
-            return api_errors(400, ['errors' => 'This product does not exist, dude!']);
-        }
-        else {
-            return api_success(['devices' => $user]);
-        }
-
-        $deviceList = DB::table('devices')->orderBy('id','ASC')->get();
-        return api_success(['data' => $deviceList],"");
-    }
 
     public function store(DevicesStoreRequest $request)
     {
         $devices = Device::create($request->all());
-
         return api_success(['data' => $devices], 200);
     }
 
