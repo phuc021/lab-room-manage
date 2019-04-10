89663a46316f91d4e47c022f273192733b0c48f2
NguyenHaiDuc <fantasy11230@gmail.com>
2019-04-10 09:42:58 +0700

    finish repository computer

--- app/Http/Controllers/Api/ComputerController.php
+++ app/Http/Controllers/Api/ComputerController.php
@@ -7,33 +7,46 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ComputerRequest;
+use App\Repositories\ComputerRepository;
 use App\Http\Requests\ComputerStoreRequest;
 use App\Http\Requests\ComputerUpdateRequest;
 
 class ComputerController extends BaseController
 {
+    private $repository;
+    public function __construct(ComputerRepository $repository)
+    {
+        $this->repository = $repository;
+    }
     public function index()
     {
         
-         $computerList = DB::table('computers')->orderby('id','ASC')->get();
-        return api_success(['data' => $computerList],"");
+        //  $computerList = DB::table('computers')->orderby('id','ASC')->get();
+        // return api_success(['data' => $computerList],"");
+        
+        return $this->repository->all();
     }
     public function store(ComputerStoreRequest $request)
     {
-        $computer = Computer::create($request->all());
-        return api_success(['data' => $computer],200);
+        // $computer = Computer::create($request->all());
+        // return api_success(['data' => $computer],200);
+        
+        return $this->repository->store($request);
     }
     public function update(ComputerUpdateRequest $request, $id)
     {
-        $computer = Computer::findOrFail($id);
-        $computer->Update($request->all());
-        return api_success(['data' => $computer],200);
-
+        // $computer = Computer::findOrFail($id);
+        // $computer->Update($request->all());
+        // return api_success(['data' => $computer],200);
+        
+        return $this->repository->update($request,$id);
     }
     public function destroy($id)
     {
-        Computer::destroy($id);
-        return api_success(['message' => 'delete computer complete'],200);
+        // Computer::destroy($id);
+        // return api_success(['message' => 'delete computer complete'],200);
+        
+        return $this->repository->destroy($id);
     }
 
 }
